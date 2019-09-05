bbac3026c74f0d336e65cc9dd07355cc5ec759d8
rqz <qiuzhang.rui@cpoao.com>
2019-09-05 16:12:18 +0800

    增加回调注释

--- cp_sdk_demo/app/src/main/java/com/cloudpoint/app/adv/sdk/demo/AdvEventListener.java
+++ cp_sdk_demo/app/src/main/java/com/cloudpoint/app/adv/sdk/demo/AdvEventListener.java
@@ -38,10 +38,21 @@ public class AdvEventListener implements IAdvertisementEvent {
 
     Handler handler = new Handler(Looper.getMainLooper());
 
+
+    /**
+     *
+     *
+     * 接收广告信息
+     *
+     * @param adv 接收到服务器推送广告
+     * @param b true:线上广告
+     */
     @Override
     public void setAdv(final Adv adv, boolean b) {
         d(adv.toString());
         //收到广告准备好的信息了。
+
+        //开始播放广告，必须在广告超时前完成播放。
         AdvPlayerHandler.getInstance().play(adv);
 //        final int seconds = 60;
 //
@@ -56,6 +67,10 @@ public class AdvEventListener implements IAdvertisementEvent {
 
     }
 
+    /**
+     *
+     * 播放器开始播放
+     */
     @Override
     public void onStart() {
 
@@ -63,6 +78,9 @@ public class AdvEventListener implements IAdvertisementEvent {
 
     }
 
+    /**
+     * 播放器播放失败
+     */
     @Override
     public void onError() {
 
@@ -70,18 +88,42 @@ public class AdvEventListener implements IAdvertisementEvent {
 
     }
 
+    /**
+     *
+     * 播放器播放结束
+     */
     @Override
     public void onEnd() {
         d(" onEnd");
     }
 
+    /**
+     *
+     *
+     *  计费结束
+     */
     @Override
     public void onCleanup() {
         d(" onCleanup");
     }
 
+    /**
+     *
+     * 通知广告状态变化，包含广告id,
+     *
+     *
+     * @param s 广告id，唯一标识
+     * @param i 广告状态
+     *
+     *          0： 播放开始
+     *          1： 播放结束
+     *          2： 计费结束
+     *          -1 或其它： 播放失败
+     */
     @Override
     public void notifyAdvTrackerStates(String s, int i) {
+
         d(" advId" + s +" , code:"+i);
+
     }
 }
