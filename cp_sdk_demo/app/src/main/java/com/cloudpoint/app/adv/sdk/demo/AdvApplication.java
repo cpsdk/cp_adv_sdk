54514e44748257bb85cee139a776b73e9bb73bb5
rqz <qiuzhang.rui@cpoao.com>
2019-09-05 15:14:12 +0800

    增加位置信息字符说明

--- cp_sdk_demo/app/src/main/java/com/cloudpoint/app/adv/sdk/demo/AdvApplication.java
+++ cp_sdk_demo/app/src/main/java/com/cloudpoint/app/adv/sdk/demo/AdvApplication.java
@@ -1,10 +1,16 @@
 package com.cloudpoint.app.adv.sdk.demo;
 
 import android.app.Application;
+import android.app.admin.DevicePolicyManager;
+import android.util.Log;
 
 import com.cloudpoint.plugins.sdk.adv.AdvPlayerHandler;
 import com.cloudpoint.plugins.sdk.adv.CPAdvSdk;
 import com.cloudpoint.shell.adv.DefaultAdvManager;
+import com.cloudpoint.shell.common.device.DeviceAddress;
+import com.cloudpoint.shell.common.device.DeviceLatitudeLongitude;
+import com.cloudpoint.shell.common.device.DeviceLocation;
+import com.google.gson.Gson;
 
 
 /**
@@ -41,11 +47,49 @@ public class AdvApplication extends Application {
             AdvEventListener advEventListener = new AdvEventListener();
             handler.setAdvertisementEvent(advEventListener);
             handler.enableManualMode(true);
+
+            /**
+             *
+             *
+             * loc['address']='中国北京市朝阳区北苑路229号'
+             loc['city']='北京市'
+             loc['country']='中国'
+             loc['disrict']='朝阳区'
+             loc['location_desc']='在金泉港附近'
+             loc['province']='北京市'
+             loc['street']='北苑路'
+             gps['coor_type']='bd09ll'
+             gps['error_code']=161
+             gps['langtitude']=116.423292
+             gps['latitude']=40.010727
+             gps['radius']=50.499428
+             */
+            DeviceAddress address =new DeviceAddress();
+            address.setAddress("中国北京市朝阳区北苑路229号");
+            address.setCity("北京市");
+            address.setDistrict("朝阳区");
+            address.setLocationDescrible("在金泉港附近");
+            address.setProvince("北京市");
+            address.setStreet("北苑路");
+
+            DeviceLatitudeLongitude gps = new DeviceLatitudeLongitude();
+            gps.setCoorType("bd09ll");
+            gps.setErrorCode(161);
+            gps.setLangtitude(116.423292);
+            gps.setLatitude(40.010727);
+            gps.setRadius(50.499428f);
+
+            DeviceLocation location = new DeviceLocation(gps,address);
+            Gson g = new Gson();
+            String loc =g.toJson(location);
+            System.out.println("loc:"+loc);
+
+            //6. 设置 location信息
+            CPAdvSdk.setLocation(loc);
         }
 
 
-        //6. 设置 location信息
-        CPAdvSdk.setLocation("");
+
 
 
     }
