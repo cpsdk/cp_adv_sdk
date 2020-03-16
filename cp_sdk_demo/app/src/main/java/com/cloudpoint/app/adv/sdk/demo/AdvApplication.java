6fa7f1c3688ad1471b5293f7299e145a57587db9
rqz <qiuzhang.rui@cpoao.com>
2020-03-16 10:18:23 +0800

    2020-03-16 增加功能wifi状态管理，获取mac地址不影响系统原来wifi开头设置

--- cp_sdk_demo/app/src/main/java/com/cloudpoint/app/adv/sdk/demo/AdvApplication.java
+++ cp_sdk_demo/app/src/main/java/com/cloudpoint/app/adv/sdk/demo/AdvApplication.java
@@ -52,35 +52,53 @@ public class AdvApplication extends Application {
             CPAdvSdk.setAdvertisementEvent(new AdvEventListener());
 
 
-            //设置位置信息
-            DeviceAddress address = new DeviceAddress();
-            address.setAddress("中国北京市朝阳区北苑路229号");
-            address.setCity("北京市");
-            address.setDistrict("朝阳区");
-            address.setLocationDescrible("在金泉港附近");
-            address.setProvince("北京市");
-            address.setStreet("北苑路");
+            if(BuildConfig.ENABLE_BAIDU_LOCATION_API){
+                locationWithBaiduApi();
+            }else{
+                locationWithoutBaiduApi();
+            }
 
-            DeviceLatitudeLongitude gps = new DeviceLatitudeLongitude();
-            gps.setCoorType("bd09ll");
-            gps.setErrorCode(161);
-            gps.setLangtitude(116.423292);
-            gps.setLatitude(40.010727);
-            gps.setRadius(50.499428f);
 
-            DeviceLocation location = new DeviceLocation(gps, address);
-            Gson g = new Gson();
-            String loc = g.toJson(location);
-            //
-            // {"location":{"address":"中国北京市朝阳区北苑路229号","city":"北京市","disrict":"朝阳区","location_desc":"在金泉港附近","province":"北京市","street":"北苑路"},"gps":{"coor_type":"bd09ll","error_code":161,"langtitude":116.423292,"latitude":40.010727,"radius":50.499428}}
-            //6. 设置 location信息
-            loc = null; // 使用集成百度定位sdk时，将loc设置为空
-            CPAdvSdk.setLocation(loc);
         }
 
 
     }
 
+
+    private void locationWithBaiduApi(){
+        //6. 设置 location信息
+        String loc = null; // 使用集成百度定位sdk时，将loc设置为空
+        CPAdvSdk.setLocation(loc);
+    }
+
+
+    private void locationWithoutBaiduApi(){
+        //设置位置信息
+        DeviceAddress address = new DeviceAddress();
+        address.setAddress("中国北京市朝阳区北苑路229号");
+        address.setCity("北京市");
+        address.setDistrict("朝阳区");
+        address.setLocationDescrible("在金泉港附近");
+        address.setProvince("北京市");
+        address.setStreet("北苑路");
+
+        DeviceLatitudeLongitude gps = new DeviceLatitudeLongitude();
+        gps.setCoorType("bd09ll");
+        gps.setErrorCode(161);
+        gps.setLangtitude(116.423292);
+        gps.setLatitude(40.010727);
+        gps.setRadius(50.499428f);
+
+        DeviceLocation location = new DeviceLocation(gps, address);
+        Gson g = new Gson();
+        String loc = g.toJson(location);
+        //
+        // {"location":{"address":"中国北京市朝阳区北苑路229号","city":"北京市","disrict":"朝阳区","location_desc":"在金泉港附近","province":"北京市","street":"北苑路"},"gps":{"coor_type":"bd09ll","error_code":161,"langtitude":116.423292,"latitude":40.010727,"radius":50.499428}}
+        //6. 设置 location信息
+        loc = null; // 使用集成百度定位sdk时，将loc设置为空
+        CPAdvSdk.setLocation(loc);
+
+    }
 
 
     @Override
